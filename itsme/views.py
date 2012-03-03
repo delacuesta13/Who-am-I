061768ab3638151560b600a82614d18b7ddcb5c6
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2012-03-02 21:05:54 -0500

    Editado texto del index.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

--- itsme/views.py
+++ itsme/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Soon the Jhon Adri&iacute;n's portfolio.")
+    return HttpResponse("Soon the Adri&aacute;n's portfolio.")
