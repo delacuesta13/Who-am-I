0a9dbd5d3934ccc511e87c415bbf86ad313369cc
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2012-03-02 21:19:51 -0500

    Editado whoisme por whoami.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

--- settings.py
+++ settings.py
@@ -18,7 +18,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'whoisme',                      # Or path to database file if using sqlite3.
+        'NAME': 'whoami',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': 'root',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
