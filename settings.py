ed62a545cf74ab1b121f88da3aa4a2fb11ac3510
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2012-03-02 19:41:29 -0500

    Agregada ruta a directorio de templates.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

--- settings.py
+++ settings.py
@@ -109,9 +109,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'whoami.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    '%s/templates/' % BASE_PATH,
 )
 
 INSTALLED_APPS = (
