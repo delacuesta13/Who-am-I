268ee2c49b7e7f9832d189b339401caedba1e5e4
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2012-03-02 19:18:29 -0500

    Agregados modelos de la aplicación.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

--- itsme/models.py
+++ itsme/models.py
@@ -1,3 +1,99 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    phone = models.CharField(max_length=60, blank=True)
+    location = models.CharField(max_length=80, blank=True)
+    profession = models.CharField(max_length=100, blank=True)
+    about = models.TextField(blank=True) # quick overview
+    resume = models.TextField(blank=True) # complete overview
+    available_for_work = models.BooleanField(default=True)
+    
+class Blog(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    site_title = models.CharField(max_length=60, blank=True)
+    tagline = models.CharField(max_length=60, blank=True)
+    
+    def __unicode__(self):
+        return self.site_title
+
+class Category(models.Model):
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(blank=True)
+    description = models.TextField(blank=True)
+    type_category_choices = (
+        ('work', 'Work'),
+        ('blog', 'Blog'),
+    )
+    type_category = models.CharField(max_length=16, choices=type_category_choices)
+    
+    def __unicode__(self):
+        return self.name
+    
+class Post(models.Model):
+    blog = models.ForeignKey(Blog)
+    categories = models.ManyToManyField(Category, through='CategoryRelationships')
+    date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    title = models.TextField(blank=True)
+    slug = models.SlugField(unique=True, blank=True)
+    content = models.TextField(blank=True)
+    status_choices = (
+        ('publish', 'Publish'),  
+        ('draft', 'Draft'),
+        ('future', 'Schedule'),                  
+    )
+    status = models.CharField(max_length=16, choices=status_choices, default="publish")
+    allow_comments = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, auto_now=True, auto_now_add=False)
+    
+    def __unicode__(self):
+        return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post)
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+    author = models.CharField(max_length=30)
+    email = models.EmailField()
+    url = models.URLField(blank=True)
+    ip = models.IPAddressField()
+    date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    content = models.TextField()
+    is_moderate = models.BooleanField(default=False)
+    is_safe = models.BooleanField(default=False) # if True, allow HTML code
+    
+class Project(models.Model):
+    user = models.ForeignKey(User)
+    categories = models.ManyToManyField(Category, through='CategoryRelationships')
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField()
+    site_url = models.URLField(blank=True)
+    
+    def __unicode_(self):
+        return self.name
+    
+class CategoryRelationships(models.Model):
+    category = models.ForeignKey(Category)
+    post = models.ForeignKey(Post, null=True, blank=True)
+    project = models.ForeignKey(Project, null=True, blank=True)
+    
+class Message(models.Model):
+    user = models.ForeignKey(User)
+    date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    ip = models.IPAddressField()
+    author = models.CharField(max_length=60)
+    email = models.EmailField()
+    subject = models.CharField(max_length=60)
+    content = models.TextField()    
+    is_readed = models.BooleanField(default=False)
+
+class Upload(models.Model):
+    user = models.ForeignKey(User)
+    path = models.TextField(blank=True)
+    title = models.TextField(blank=True)
+    upload_date = models.DateTimeField(null=True, auto_now=False, auto_now_add=True)
+    extension_file = models.CharField(max_length=60, blank=True)
+    description = models.TextField(blank=True)
