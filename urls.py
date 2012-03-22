bba5450df47c4eff99e8830c1615b0e4a817a90a
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2012-03-21 20:34:07 -0500

    Agregada url de vista que muestra el contenido de un determinado post.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

--- urls.py
+++ urls.py
@@ -3,4 +3,6 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
     url(r'^admin/', include('admin.urls')),
     url(r'^$', 'itsme.views.index'),
+    url(r'^page/(?P<page>\d+)/$', 'itsme.views.index'),
+    url(r'^blog/(?P<slug>[\w-]+)/$', 'itsme.views.post_view')
 )
