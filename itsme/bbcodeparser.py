6d823fbccdb712c22008d04ae127da8d3ea67994
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2012-03-27 17:42:21 -0500

    Agregadas más reglas para bbcode.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

--- itsme/bbcodeparser.py
+++ itsme/bbcodeparser.py
@@ -1,60 +1,122 @@
 # -*- coding: iso-8859-1 -*-
 
 import re
+from django.template.defaultfilters import force_escape
 
 class BBCodeParser:
 
     bbcode_rules = {
-            'heading': {
-                        'sub': [r'\[h([1-6]{1})\](.*?)\[/h([1-6]{1})\]',
-                                r'<h\1>\2</h\1>', r'\2'],
-                        },
-            'paragraph': {
-                          'sub': [r'\[p\](.*?)\[/p\]', r'<p>\1</p>', r'\1'],
-                          'findall': r'\[p\](.*?)\[/p\]'
-                          },
-            'color': {
-                      'sub': [r'\[color=(#[0-9a-fA-F]{3,6}|[a-zA-Z]+)\](.*?)\[/color\]',
-                              r'<span style="color: \1;">\2</span>', r''], 
+        'heading': {
+                    'sub': [
+                            r'\[h([1-6]{1})\](.*?)\[/h([1-6]{1})\]', # Regex to find BBCode tag
+                            r'<h\1>\2</h\1>', # Replace from BBCode to HTML
+                            r'\2', # Remove BBCode
+                            ],
+                    },
+        'paragraph': {
+                      'sub': [
+                              r'\[p\](.*?)\[/p\]', 
+                              r'<p>\1</p>', 
+                              r'\1',
+                              ],
+                      'findall': r'\[p\](.*?)\[/p\]', # find in content this BBCode tag
                       },
-            'size': {
-                     'sub': [r'\[size=(\d+)\](.*?)\[/size\]',
-                             r'<span style="font-size: \1px;">\2</span>',
-                             r''],
-                     },
-            'bold': {
-                     'sub': [r'\[b\](.*?)\[/b\]', r'<strong>\1</strong>',
-                             r'\1'],
-                     },
-            'italic': {
-                       'sub': [r'\[i\](.*?)\[/i\]', r'<em>\1</em>',
-                               r'\1'],
-                       },
-            'Stroke through': {
-                               'sub': [r'\[del\](.*?)\[/del\]', r'<del>\1</del>',
-                                       r'\1'],
-                               },
-            'link': {
-                     'sub': [r'\[url=(.*?)\](.+?)\[/url\]', r'<a href="\1">\2</a>',
-                             r'\2'],
-                     },
-            'youtube': {
-                        'sub': [r'\[youtube\s+width=(\d*?)\s+height=(\d*?)\](.*?)\[/youtube\]',
-                                r'<div class="video-container"><iframe width="\1" height="\2" src="http://www.youtube.com/embed/\3" frameborder="0"></iframe></div>',
-                                r''],
-                        },
-            'vimeo': {
-                      'sub': [r'\[vimeo\s+width=(\d*?)\s+height=(\d*?)\](.*?)\[/vimeo\]',
-                              r'<div class="video-container"><iframe src="http://player.vimeo.com/video/\3?byline=0&amp;portrait=0&amp;color=707070" width="\1" height="\2" frameborder="0"></iframe><div>',
-                              r'']
-                      },        
+        'color': {
+                  'sub': [
+                          r'\[color=(#[0-9a-fA-F]{3,6}|[a-zA-Z]+?)\](.*?)\[/color\]',
+                          r'<span style="color: \1;">\2</span>', 
+                          r'',
+                          ],
+                  },
+        'size': {
+                 'sub': [
+                         r'\[size=(\d+?)\](.*?)\[/size\]',
+                         r'<span style="font-size: \1px;">\2</span>',
+                         r'',
+                         ],
+                 },
+        'bold': {
+                 'sub': [
+                         r'\[b\](.*?)\[/b\]', 
+                         r'<strong>\1</strong>',
+                         r'\1',
+                         ],
+                 },
+        'italic': {
+                   'sub': [
+                           r'\[i\](.*?)\[/i\]', 
+                           r'<em>\1</em>',
+                           r'\1',
+                           ],
+                   },
+        'Stroke through': {
+                           'sub': [
+                                   r'\[del\](.*?)\[/del\]', 
+                                   r'<del>\1</del>',
+                                   r'\1',
+                                   ],
+                           },
+        'center': {
+                   'sub': [
+                           r'\[center\](.*?)\[/center\]',
+                           r'<div style="text-align: center;">\1</div>',
+                           r'\1',
+                           ],
+                   },
+        'quote': {
+                  'sub': [
+                          r'\[quote=(.*)\](.*?)\[/quote\]',
+                          r'<blockquote><p>\2</p><small>\1</small></blockquote>',
+                          r'\2',
+                          ],
+                  },
+        'quote without autor': {
+                  'sub': [
+                          r'\[quote](.*?)\[/quote\]',
+                          r'<blockquote><p>\1</p></blockquote>',
+                          r'\1',
+                          ],
+                  },
+        'code': {
+                 'sub': [
+                         r'\[code\](.*?)\[/code\]',
+                         r'<pre class="prettyprint linenums" style="margin-bottom: 9px;">\1</pre>',
+                         r'\1'
+                         ],
+                 },
+        'link': {
+                 'sub': [
+                         r'\[url=(.*?)\](.*?)\[/url\]', 
+                         r'<a href="\1">\2</a>',
+                         r'\2',
+                         ],
+                 },
+        'youtube': {
+                    'sub': [
+                            r'\[youtube\s+width=(\d*?)\s+height=(\d*?)\](.*?)\[/youtube\]',
+                            r'<div class="video-wrapper">\n\t<div class="video-container">\n\t\t<iframe src="http://www.youtube.com/embed/\3" width="\1" height="\2" frameborder="0" allowFullScreen></iframe>\n\t</div>\n</div>',
+                            r'',
+                            ],
+                    },
+        'vimeo': {
+                  'sub': [
+                          r'\[vimeo\s+width=(\d*?)\s+height=(\d*?)\](.*?)\[/vimeo\]',
+                          r'<div class="video-wrapper">\n\t<div class="video-container">\n\t\t<iframe src="http://player.vimeo.com/video/\3?byline=0&amp;portrait=0" width="\1" height="\2" frameborder="0" allowFullScreen></iframe>\n\t</div>\n</div>',
+                          r'',
+                          ],
+                  },        
     }
     
     def __init__(self, bbcode):
         self.bbcode = bbcode
-        
-    def bbcode_to_html(self):
-        s = self.bbcode
+    
+    def escape_html(self, s=''):
+        """Return an escaped string of HTML."""
+        escaped_str = s if len(s) > 0 else self.bbcode
+        return force_escape(escaped_str)
+    
+    def bbcode_to_html(self, s=''):
+        s = s if len(s) > 0 else self.bbcode
         for value in self.bbcode_rules.itervalues():
             re_bbcode = re.compile(value['sub'][0], re.MULTILINE|re.DOTALL|re.IGNORECASE) 
             s = re_bbcode.sub(value['sub'][1], s)
@@ -63,8 +125,8 @@ class BBCodeParser:
     def get_all_paragraphs(self):
         return re.findall(self.bbcode_rules['paragraph']['findall'], self.bbcode)
     
-    def remove_bbcode(self, s, *tags):
-        without_bbcode = s
+    def remove_bbcode(self, s='', *tags):
+        without_bbcode = s if len(s) > 0 else self.bbcode
         if len(tags) != 0:
             for tag in tags:
                 if tag in self.bbcode_rules:
@@ -75,4 +137,3 @@ class BBCodeParser:
                 re_bbcode = re.compile(value['sub'][0], re.MULTILINE|re.DOTALL|re.IGNORECASE)
                 without_bbcode = re_bbcode.sub(value['sub'][2], without_bbcode)
         return without_bbcode 
-        
