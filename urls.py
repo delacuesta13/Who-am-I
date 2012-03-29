ac082454f67c3766d0ba7d3ea2cb3564510eedff
Jhon Adrián Cerón <jadrian.ceron@gmail.com>
2012-03-29 18:21:59 -0500

    Agregada URL de Feed RSS.
    Signed-off-by: Jhon Adrián Cerón <jadrian.ceron@gmail.com>

--- urls.py
+++ urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
+from rss.feeds import RSSFeed
 
 urlpatterns = patterns('',
     url(r'^admin/', include('admin.urls')),
     url(r'^$', 'itsme.views.index'),
+    url(r'^feed/$', RSSFeed()),
     url(r'^page/(?P<page>\d+)/$', 'itsme.views.index'),
     url(r'^blog/(?P<slug>[\w-]+)/$', 'itsme.views.post_view')
 )
